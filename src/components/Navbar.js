6b6440725e1f4a0f13ddbaaf7924317b7b74f20c
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-10-21 11:57:10 +0200

    create navbar and booklist components

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+// import React from 'react'
+
+// const Navbar = () => {
+// 	return (
+// 		<div>
+				
+// 		</div>
+// 	)
+// }
+
+// export default Navbar
+
+
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+	render() {
+		return (
+			<nav>
+				<h1>Context App</h1>
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+		)
+	}
+}
+
+export default Navbar
