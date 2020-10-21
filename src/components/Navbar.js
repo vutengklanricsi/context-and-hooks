de51436c207d84b0579e3e3941f21eadf3fa8106
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-10-21 13:57:05 +0200

    rewrtie navbar components into functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
-class Navbar extends Component {
-	static contextType = ThemeContext
-  render() {
-		// console.log(this.context);
-		const {isLightTheme, light, dark} = this.context;
-		const theme = isLightTheme ? light : dark;
-		return (
-      <nav style={{background: theme.ui, color: theme.syntax}}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    );
-  }
-}
+// class Navbar extends Component {
+// 	static contextType = ThemeContext
+//   render() {
+// 		// console.log(this.context);
+// 		const {isLightTheme, light, dark} = this.context;
+// 		const theme = isLightTheme ? light : dark;
+// 		return (
+//       <nav style={{background: theme.ui, color: theme.syntax}}>
+//         <h1>Context App</h1>
+//         <ul>
+//           <li>Home</li>
+//           <li>About</li>
+//           <li>Contact</li>
+//         </ul>
+//       </nav>
+//     );
+//   }
+// }
+const Navbar = () => {
+  return (
+    <ThemeContext.Consumer>
+      {(context) => {
+        const { isLightTheme, light, dark } = context;
+				const theme = isLightTheme ? light : dark;
+        return (
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
+};
 
 export default Navbar;
