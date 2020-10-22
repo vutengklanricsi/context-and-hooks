afb130adefc1e1b60b528c276d359f43d10bd62c
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-10-22 13:36:45 +0200

    rewriting some code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ThemeContext } from "../context/ThemeContext";
 
@@ -21,31 +21,26 @@ import { ThemeContext } from "../context/ThemeContext";
 //   }
 // }
 const Navbar = () => {
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+
+  const theme = isLightTheme ? light : dark;
+
+  const ChangeLogin = () => {
+    return toggleAuth;
+  };
   return (
-    <AuthContext.Consumer>{(AuthContext) => {
-      const {isAuthenticated, toggleAuth} = AuthContext;
-      const ChangeLogin = () => {
-        return toggleAuth;
-      }
-      return(
-      <ThemeContext.Consumer>{(ThemeContext) => {
-        const { isLightTheme, light, dark } = ThemeContext;
-				const theme = isLightTheme ? light : dark;
-        return(
-          <nav style={{ background: theme.ui, color: theme.syntax }}>
-            <h1>Context App</h1>
-            <div onClick={ChangeLogin()}>
-              {isAuthenticated ? 'logged in' : 'logged out'}
-            </div>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        );
-      }}</ThemeContext.Consumer>
-      )}}</AuthContext.Consumer>
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div onClick={ChangeLogin()}>
+        {isAuthenticated ? "logged in" : "logged out"}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   );
 };
 
